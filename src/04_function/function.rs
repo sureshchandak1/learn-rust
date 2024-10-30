ff1d954bb4c91281fb0ef50611f64b2db785b898
SSureshC <sureshchandak1@gmail.com>
2024-10-30 13:51:44 +0530

    Functions

--- src/04_function/function.rs
+++ src/04_function/function.rs
@@ -1,3 +1,15 @@
 fn main() {
-    
+
+    print("Hello world");
+    print("Ramesh");
+
+    println!("{}", add(10, 20));
+}
+
+fn print(message: &str) {
+    println!("{}", message);
+}
+
+fn add(v1: i16, v2: i16) -> i16 {
+    return v1 + v2;
 }
