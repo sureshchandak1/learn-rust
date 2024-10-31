191bae33b6135cf45611f889ecbcb2afb64cf1a0
SSureshC <sureshchandak1@gmail.com>
2024-10-31 18:07:52 +0530

    Shadowing

---
+++ src/09_shadowing/shadowing.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    let x = 5; // integer
+    println!("x = {}", x);
+
+    let x = "Hello World"; // string
+    println!("x = {}", x);
+
+    let x = x.len(); // integer
+    println!("x = {}", x);
+}
